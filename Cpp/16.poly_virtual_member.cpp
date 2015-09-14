370966f604efe777cf5818f96e4a57307371cd44
cliu <fluency.03@gmail.com>
2015-09-14 09:15:37 +0200

    virtual member

---
+++ Cpp/16.poly_virtual_member.cpp
@@ -0,0 +1,52 @@
+// derived classes
+#include <iostream>
+using namespace std;
+
+
+class CPolygon{
+protected: // what do the protected, private, and public mean?
+	int width, height;
+public:
+	void set_values(int a, int b)
+		{ width = a; height = b; }
+	// allow CRectangle and CTriangle to implement 
+	// different versions of area calculation
+	virtual int area()
+		{ return 0;}
+};
+
+class CRectrangle: public CPolygon {
+public:
+	int area()
+		{return ( width * height );}
+};
+
+class CTriangle: public CPolygon {
+public:
+	int area()
+		{return ( width * height / 2 );}
+};
+
+int main(){
+	CRectrangle rect;
+	CTriangle trgl;
+	CPolygon poly;
+
+	CPolygon * ppoly1 = &rect; 
+	CPolygon * ppoly2 = &trgl;
+	CPolygon * ppoly3 = &poly;
+	
+	ppoly1->set_values(4,5);
+	ppoly2->set_values(4,5);
+	ppoly3->set_values(4,5);
+
+	cout << ppoly1->area() << endl;
+	cout << ppoly2->area() << endl;
+	cout << ppoly3->area() << endl;
+
+	return 0;
+}
+
+
+
+
