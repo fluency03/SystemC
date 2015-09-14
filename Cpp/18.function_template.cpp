f8072b6e6ca3ed7c8caaca3bea8279a32daf4061
cliu <fluency.03@gmail.com>
2015-09-14 12:50:45 +0200

    function template

--- Cpp/18.function_template.cpp
+++ Cpp/18.function_template.cpp
@@ -10,11 +10,21 @@ T GetMax (T a, T b){
 }
 
 int main() {
+	
 	int i=5, j=6, k;
 	long l=10, m=5, n;
+
 	k = GetMax<int>(i,j);
 	n = GetMax<long>(l,m);
+
+// type is not needed. the following works as well. 
+	// k = GetMax(i,j);
+	// n = GetMax(l,m);
+
+	cout << k << endl;
+	cout << n << endl;
 	
+	return 0;
 }
 
 
