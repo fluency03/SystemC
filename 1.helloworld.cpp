4797c2740295fd83134fda6ae659db4f1f06609d
cliu <fluency.03@gmail.com>
2015-09-14 17:25:53 +0200

    helloworld.cpp

--- 1.helloworld.cpp
+++ 1.helloworld.cpp
@@ -1,6 +1,10 @@
 #include "systemc.h"
 // hello world
 
+// SC_MODULE defines a Class
+// SC_CTOR declares a class constructor
+
+// hello_world is the module name. 
 SC_MODULE (hello_world) 
 {
 	SC_CTOR (hello_world){}
@@ -11,9 +15,12 @@ SC_MODULE (hello_world)
 	}
 };
 
+// To process command-line arguments.
+// argc: argument count
+// argv: argument vector
 int sc_main(int argc, char* argv[])
 {
-	hello_world hello("HELLO");
+	hello_world hello("HELLO"); // why there is a "HELLO"
 	hello.say_world();
 	return (0);
 }
