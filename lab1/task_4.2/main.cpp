1da85197a4fe17b300f1a99a0623d3ad8897a29f
cliu <fluency.03@gmail.com>
2015-09-16 09:28:08 +0200

    main.cpp

--- lab1/task_4.2/main.cpp
+++ lab1/task_4.2/main.cpp
@@ -5,15 +5,41 @@
 #include "sink.h"
 #include "systemc.h"
 
-int sc_main(int argc, char *argv[]){
+class top : public sc_module
+{
+
+public:
+	hw_fifo_wrapper <char> fifo_inst;
+	source source1;
+	sink sink1;
+	sc_clock clk;
 
 
+	top(sc_module_name name, unsigned size)
+		: sc_module(name),
+		  fifo_inst("FIFO1", size),
+		  source1("source1"),
+		  sink1("sink1"),
+		  clk("c1", 1, SC_NS)
+	{
+		source1.out(fifo_inst);
+		sink1.in(fifo_inst);
+		fifo_inst.clk(clk);
+	}
+
+};
 
 
+int sc_main(int argc, char *argv[])
+{
 
+	unsigned size = 30;
 
+	top top1("Top1", size);
 
+	sc_start(1000, SC_NS);
 
+	cout << endl << endl;
 
 	return 0;
 
