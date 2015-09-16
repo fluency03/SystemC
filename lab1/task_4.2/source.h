23a3afd65311324e64229a315adaff8a2252d994
cliu <fluency.03@gmail.com>
2015-09-16 09:27:53 +0200

    source

--- lab1/task_4.2/source.h
+++ lab1/task_4.2/source.h
@@ -1,19 +1,19 @@
 // file name = source.h
 
 #include "systemc.h"
-#include "channel.h"
+// #include "channel.h"
 
 class source : public sc_module
 {
 
 public:
-    sc_port<w_if> out;
+    sc_port< sc_fifo_out_if<char> > out;
 
     SC_HAS_PROCESS(source);
 	
     source(sc_module_name name) : sc_module(name)
     {
-       
+       SC_THREAD(source_p);
     }
 
     void source_p();
