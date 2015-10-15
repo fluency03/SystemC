4ec34c471bc3d40aac1488b02b68d9abd8c8f8ca
cliu <fluency.03@gmail.com>
2015-10-15 23:45:35 +0200

    mem_slave

---
+++ lab3/TLM-2005-04-08/examples/user_4node/mem_slave.cc
@@ -0,0 +1,87 @@
+
+/*****************************************************************************
+
+  The following code is derived, directly or indirectly, from the SystemC
+  source code Copyright (c) 1996-2004 by all Contributors.
+  All Rights reserved.
+
+  The contents of this file are subject to the restrictions and limitations
+  set forth in the SystemC Open Source License Version 2.4 (the "License");
+  You may not use this file except in compliance with such restrictions and
+  limitations. You may obtain instructions on how to receive a copy of the
+  License at http://www.systemc.org/. Software distributed by Contributors
+  under the License is distributed on an "AS IS" basis, WITHOUT WARRANTY OF
+  ANY KIND, either express or implied. See the License for the specific
+  language governing rights and limitations under the License.
+
+ *****************************************************************************/
+
+
+#include "mem_slave.h"
+
+using user_4node::mem_slave;
+
+using basic_protocol::basic_status;
+
+mem_slave::mem_slave( sc_module_name module_name , int k ) :
+  sc_module( module_name ) ,
+  target_port0("tport0"), target_port1("tport1"),
+  target_port2("tport2"), target_port3("tport3"),
+  initiator_port0("iport0"), initiator_port1("iport1"),
+  initiator_port2("iport2"), initiator_port3("iport3"),
+  read_num(0), write_num(0)
+{
+
+  target_port0( *this );
+  target_port1( *this );
+  target_port2( *this );
+  target_port3( *this );
+
+  memory = new ADDRESS_TYPE[ k * 1024 ];
+
+}
+
+basic_status mem_slave::transfer( const ADDRESS_TYPE &a , DATA_TYPE &d )
+{
+  switch (a) {
+    case 0:
+      initiator_port0.write( d );
+      break;
+    case 1:
+      initiator_port1.write( d );
+      break;
+    case 2:
+      initiator_port2.write( d );
+      break;
+    case 3:
+      initiator_port3.write( d );
+    default:
+      break;
+  }
+  // read_num ++; // number of read counts up by 1
+  return basic_protocol::SUCCESS;
+}
+
+basic_status mem_slave::write( const ADDRESS_TYPE &a , const DATA_TYPE &d )
+{
+
+  // cout << name() << " writing @ " << a << " value " << d << endl; 
+  memory[a] = d;
+  write_num ++; // number of write counts up by 1
+  return basic_protocol::SUCCESS;
+}
+
+basic_status mem_slave::read( const ADDRESS_TYPE &a , DATA_TYPE &d )
+{
+
+  d = memory[a];
+  // cout << name() << " reading from " << a << " value " << d << endl;
+  read_num ++; // number of read counts up by 1
+  return basic_protocol::SUCCESS;
+}
+ 
+mem_slave::~mem_slave() {
+
+  delete [] memory;
+
+}
---
+++ lab3/TLM-2005-04-08/examples/user_4node/mem_slave.h
@@ -0,0 +1,73 @@
+/*****************************************************************************
+
+  The following code is derived, directly or indirectly, from the SystemC
+  source code Copyright (c) 1996-2004 by all Contributors.
+  All Rights reserved.
+
+  The contents of this file are subject to the restrictions and limitations
+  set forth in the SystemC Open Source License Version 2.4 (the "License");
+  You may not use this file except in compliance with such restrictions and
+  limitations. You may obtain instructions on how to receive a copy of the
+  License at http://www.systemc.org/. Software distributed by Contributors
+  under the License is distributed on an "AS IS" basis, WITHOUT WARRANTY OF
+  ANY KIND, either express or implied. See the License for the specific
+  language governing rights and limitations under the License.
+
+ *****************************************************************************/
+
+
+#ifndef MEM_SLAVE_HEADER
+#define MEM_SLAVE_HEADER
+
+#include "systemc.h"
+
+#include "bus_types.h"
+#include "basic_initiator_port.h"
+#include "basic_protocol.h"
+#include "basic_slave_base.h"
+
+using basic_protocol::basic_status;
+using basic_protocol::basic_slave_base;
+using tlm::tlm_transport_if;
+
+namespace user_4node
+{
+
+class mem_slave :
+  public sc_module ,
+  public virtual basic_slave_base< ADDRESS_TYPE , DATA_TYPE >
+{
+public:
+  mem_slave( sc_module_name module_name , int k = 1 );
+
+  sc_export< if_type > target_port0;
+  sc_export< if_type > target_port1;
+  sc_export< if_type > target_port2;
+  sc_export< if_type > target_port3;
+
+  basic_initiator_port<ADDRESS_TYPE, DATA_TYPE> initiator_port0;
+  basic_initiator_port<ADDRESS_TYPE, DATA_TYPE> initiator_port1;
+  basic_initiator_port<ADDRESS_TYPE, DATA_TYPE> initiator_port2;
+  basic_initiator_port<ADDRESS_TYPE, DATA_TYPE> initiator_port3;
+
+  basic_status write( const ADDRESS_TYPE & , const DATA_TYPE & );
+  basic_status read( const ADDRESS_TYPE & , DATA_TYPE & );
+  basic_status transfer( const ADDRESS_TYPE & , DATA_TYPE & );
+
+  ~mem_slave();
+
+  // return number of read and write transaction through mem_slave
+  unsigned int read_out() {return read_num;};
+  unsigned int write_out() {return write_num;};
+
+  unsigned int read_num, write_num;
+
+private:
+  ADDRESS_TYPE *memory;
+
+
+};
+
+};
+
+#endif
