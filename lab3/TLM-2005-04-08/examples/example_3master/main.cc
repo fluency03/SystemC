610d8199409d55e3c4e724a5c05ec8f8f5a38aeb
cliu <fluency.03@gmail.com>
2015-10-15 23:42:11 +0200

    main

--- lab3/TLM-2005-04-08/examples/example_3master/main.cc
+++ lab3/TLM-2005-04-08/examples/example_3master/main.cc
@@ -17,13 +17,9 @@
 
 
 #include "master1.h"
-#include "master2.h"
-#include "master3.h"
 #include "mem_slave.h"
 
 using user_3master::master1;
-using user_3master::master2;
-using user_3master::master3;
 using user_3master::mem_slave;
 
 
@@ -33,95 +29,22 @@ public:
   sc_in<bool> clk;
 
   top( sc_module_name module_name ):
-    sc_module( module_name ), 
-    m1("master1"), m2("master2"), m3("master3"), s("slave")
+    sc_module( module_name ), m1("master1", 1), m2("master2", 2), m3("master3", 3), s("slave")
     {
 
       m1.initiator_port( s.target_port1 );
       m2.initiator_port( s.target_port2 );
       m3.initiator_port( s.target_port3 );
 
-      SC_THREAD(run1);
-        sensitive << clk.pos();
-        // dont_initialize();
-      SC_THREAD(run2);
-        sensitive << clk.pos();
-        // dont_initialize();
-      SC_THREAD(run3);
-        sensitive << clk.pos();
-        // dont_initialize();
+      m1.clk(clk);
+      m2.clk(clk);
+      m3.clk(clk);
 
     }
-
-  SC_HAS_PROCESS( top );
-
-  // sc_in<bool> Trig;
 
 private:
-  master1 m1;
-  master2 m2;
-  master3 m3;
+  master1 m1, m2, m3;
   mem_slave s; 
-
-  void run1()
-  {
-
-    DATA_TYPE d = 1;
-    unsigned int i = 0;
-
-    // cout << " ====================== MASTER1 ======================= " << endl; 
-    // cout << " =============== This is data WRITING!! =============== " << endl; 
-    for( i = 0; i < 20; i++ )
-    {
-
-      cout << " ====================== MASTER1 ======================= " << endl; 
-      cout << "Writing Address " << i << " value " << d << endl;
-      m1.initiator_port.write( i , d );
-      wait();
-
-    }
-
-  }
-
-  void run2()
-  {
-
-    DATA_TYPE d = 2;
-    unsigned int i = 0;
-
-    // cout << " ====================== MASTER2 ======================= " << endl; 
-    // cout << " =============== This is data WRITING!! =============== " << endl; 
-    for( i = 0; i < 20; i++ )
-    {
-
-      cout << " ====================== MASTER2 ======================= " << endl;
-      cout << "Writing Address " << i << " value " << d << endl;
-      m2.initiator_port.write( i , d );
-      wait();
-
-    }
-
-  }
-
-  void run3()
-  {
-
-    DATA_TYPE d;
-    unsigned int i = 0;
-
-    // cout << " ====================== MASTER3 ======================= " << endl; 
-    // cout << " =============== This is data READING!! =============== " << endl;
-    for( i = 0; i < 20; i++ )
-    {
-
-      m3.initiator_port.read( i , d );
-      cout << " ====================== MASTER3 ======================= " << endl; 
-      cout << "Read Address " << i << " got " << d << endl;
-      wait();
-
-    }
-  }
-
 
 };
 
@@ -130,26 +53,9 @@ private:
 int sc_main( int argc , char **argv )
 {
 
-  // master1 m1("master1");
-  // master2 m2("master2");
-  // master3 m3("master3");
-  // mem_slave s("slave");
-
-  // m1.initiator_port( s.target_port1 );
-  // m2.initiator_port( s.target_port2 );
-  // m3.initiator_port( s.target_port3 );
-
-  // bool b = true;
-  // unsigned int i;
-
   sc_clock clk("c1", 1, SC_NS);
   top top1("top1");
   top1.clk(clk);
-
-
-  // for( i = 0; i < 20; i++ ){
-    // b = !b;
-  // }
 
   sc_start( 100, SC_NS );
 
